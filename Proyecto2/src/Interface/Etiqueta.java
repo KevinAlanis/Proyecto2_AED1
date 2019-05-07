442cd16678bbbcec0f24d6d11221a5aca1a3dd7d
Isaac4918 <isaacarayabusiness@hotmail.com>
2019-05-06 19:28:32 -0600

    Avance de pantalla de registro y añadidas distintas clases nuevas de creación de interfaz

--- Proyecto2/src/Interface/Etiqueta.java
+++ Proyecto2/src/Interface/Etiqueta.java
@@ -3,32 +3,27 @@ package Interface;
 import javax.swing.*;
 import java.awt.*;
 
-public class Etiqueta {
+public class Etiqueta extends JLabel{
     Color Fondo = new Color(190,190,190);
     int sizeX = 80;
     int sizeY = 20;
     int posx;
     int posy;
     String text = "";
-    Pantalla Window;
 
-    public Etiqueta(Pantalla window, int sizeX, int sizeY, int posx, int posy, String text) {
+    public Etiqueta(int sizeX, int sizeY, int posx, int posy, String text) {
         this.sizeX = sizeX;
         this.sizeY = sizeY;
         this.posx = posx;
         this.posy = posy;
         this.text = text;
-        this.Window = window;
+        setText(text);
+        setSize(this.sizeX,this.sizeY);
+        setVisible(true);
+        setBounds(this.posx, this.posy, this.sizeX, this.sizeY);
+        setFont(new Font("Impact", 1, 40));
+        setForeground(Color.WHITE);
+        setBackground(this.Fondo);
     }
 
-    public void colocar(){
-        JLabel L1 = new JLabel(text);
-        L1.setSize(this.sizeX,this.sizeY);
-        L1.setVisible(true);
-        L1.setBounds(this.posx, this.posy, this.sizeX, this.sizeY);
-        L1.setFont(new Font("Impact", 1, 40));
-        L1.setForeground(Color.WHITE);
-        L1.setBackground(this.Fondo);
-        this.Window.add(L1);
-    }
 }
---
+++ Proyecto2/src/Interface/Tabla.java
@@ -0,0 +1,19 @@
+package Interface;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener;
+
+public class Tabla extends JTable {
+    Color b1 = new Color(255,255,255);
+
+
+    public Tabla(){
+    }
+
+
+}
